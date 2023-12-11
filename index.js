2f12d4c5534c98c2ad92e6b39bf87122d1b86dc6
Thimo <thimodehaan@gmail.com>
2023-12-11 17:28:20 +0100

    added json response

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(JSON.parse(fs.readFileSync("./data/examplecars.json")))
 })
 
 app.use('/cars', carRoutes)
