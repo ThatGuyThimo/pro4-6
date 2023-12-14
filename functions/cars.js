5fc1fbad61224e28d57fca3812527e4c629bed50
Thimo <thimodehaan@gmail.com>
2023-12-14 16:47:00 +0100

    added pagenation and link to self for collection

--- functions/cars.js
+++ functions/cars.js
@@ -1,139 +1,148 @@
-import { dbPostCar, dbGetCars, dbGetBrands, dbGetOneCar } from "../components/dbFunctions.js";
-import { Car, Brand } from '../components/database.js'
-import { logError } from "./errorLogging.js"
-
+import {
+  dbPostCar,
+  dbGetCars,
+  dbGetBrands,
+  dbGetOneCar,
+} from "../components/dbFunctions.js";
+import { Car, Brand } from "../components/database.js";
+import { logError } from "./errorLogging.js";
 
 async function addCar(req, res) {
-    try {
-        const car = new Car(req.body)
+  try {
+    const car = new Car(req.body);
 
-        const validation = car.validateSync()
+    const validation = car.validateSync();
 
-        if(validation) {
-            res.status(400).json({error: 'Parameters must be filled', info: validation.errors})
-            return
-        }
-
-        const response = dbPostCar(car)
-
-        res.status(201).json(response)
-    } catch(e) {
-        console.log("addCar ", await logError("addCar", e))
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (validation) {
+      res
+        .status(400)
+        .json({ error: "Parameters must be filled", info: validation.errors });
+      return;
     }
+
+    const response = dbPostCar(car);
+
+    res.status(201).json(response);
+  } catch (e) {
+    console.log("addCar ", await logError("addCar", e));
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 async function getAllCars(req, res) {
-    try {
-        const cars = await dbGetCars()
+  try {
+    const cars = await dbGetCars();
 
-        let carArray = []
+    let carArray = [];
 
-        cars.forEach(car => {
-            carArray.push(links(car))
-        });
+    cars.forEach((car) => {
+      carArray.push(links(car));
+    });
 
-        // carArray.push({_links:{self: {
-        //     href: 'https://thimodehaan.com:8080/cars'
-        // }}})
+    const linkToSelf = {self: {
+        href: 'https://thimodehaan.com:8080/cars'
+    }}
 
-        
-        // console.log(pagination(carArray))
-        const data = {items: pagination(carArray)}
-        
-        res.json(data)
-    } catch(e) {
-        console.log("getAllCars ", await logError("getAllCars", e))
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
+    // console.log(pagination(carArray))
+    const data = { items: carArray,_link: linkToSelf, pagination: pagination(carArray) };
+    console.log(data);
+
+    res.json(data);
+  } catch (e) {
+    console.log("getAllCars ", await logError("getAllCars", e));
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 
 async function getOneCar(req, res) {
-    if(await checkParams(req, res)) {
-        try {
-            let car = await dbGetOneCar(req.query)
-            const data = links(car)
+  if (await checkParams(req, res)) {
+    try {
+      let car = await dbGetOneCar(req.query);
+      const data = links(car);
 
-            res.json(data)
-        } catch(e) {
-            console.log("getOneCar ", await logError("getOneCar", e))
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
+      res.json(data);
+    } catch (e) {
+      console.log("getOneCar ", await logError("getOneCar", e));
+      res.status(500).json({ error: "Internal Server Error" });
     }
-};
+  }
+}
 
 async function getAllBrands(req, res) {
-    try {
-        const data = await dbGetBrands()
-        res.json(data)
-    } catch(e) {
-        console.log("getAllBrands ", await logError("getAllBrands", e))
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
+  try {
+    const data = await dbGetBrands();
+    res.json(data);
+  } catch (e) {
+    console.log("getAllBrands ", await logError("getAllBrands", e));
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 
 async function checkParams(req, res) {
-    if (Object.keys(req.query) == 0) {
-        res.status(400).json({ error: "No value given!"})
-        return false
-    } else {
-        return true
-    }
-};
+  if (Object.keys(req.query) == 0) {
+    res.status(400).json({ error: "No value given!" });
+    return false;
+  } else {
+    return true;
+  }
+}
 
 function validateObj(obj) {
-    return new Promise((resolve, reject) => {
-    let index = 0
-        Object.values(obj).forEach(value => {
-            index++
-            if(value == null || value == "" || value == undefined ||  value == 'undefined' || value == "NaN") {
-                resolve(false)
-            } else if (index === Object.values(obj).length) {
-                console.log("ran")
-                resolve(true)
-            }
-        });
-    })
-  }
+  return new Promise((resolve, reject) => {
+    let index = 0;
+    Object.values(obj).forEach((value) => {
+      index++;
+      if (
+        value == null ||
+        value == "" ||
+        value == undefined ||
+        value == "undefined" ||
+        value == "NaN"
+      ) {
+        resolve(false);
+      } else if (index === Object.values(obj).length) {
+        console.log("ran");
+        resolve(true);
+      }
+    });
+  });
+}
 
-  function links(car) {
-    return {
-        ...car.toObject(),
-        _links: {
-          self: {
-            href: `https://thimodehaan.com:8080/cars/detail/?_id=${car._id}`,
-          },
-          collection: {
-            href: `https://thimodehaan.com:8080/cars`,
-          },
-        },
-      };
-  }
+function links(car) {
+  return {
+    ...car.toObject(),
+    _links: {
+      self: {
+        href: `https://thimodehaan.com:8080/cars/detail/?_id=${car._id}`,
+      },
+      collection: {
+        href: `https://thimodehaan.com:8080/cars`,
+      },
+    },
+  };
+}
 
-  function pagination(obj) {
-    return {
-        ...obj,
-        pagination: {
-            currentPage: 1,
-            currentItems: obj.length,
-            totalPages: 1,
-            totalItems: 20,
-            _links: {
-                first: {
-                    href: `https://thimodehaan.com:8080/cars/detail/`
-                },
-                last: {
-                    href: `https://thimodehaan.com:8080/cars/detail/`
-                },
-                previous: {
-                    href: `https://thimodehaan.com:8080/cars/detail/`
-                },
-                next: {
-                    href: `https://thimodehaan.com:8080/cars/detail/`
-                }
-            }
-        }
-      };
-  }
+function pagination(obj) {
+  return {
+    currentPage: 1,
+    currentItems: obj.length,
+    totalPages: 1,
+    totalItems: 20,
+    _links: {
+      first: {
+        href: `https://thimodehaan.com:8080/cars/detail/`,
+      },
+      last: {
+        href: `https://thimodehaan.com:8080/cars/detail/`,
+      },
+      previous: {
+        href: `https://thimodehaan.com:8080/cars/detail/`,
+      },
+      next: {
+        href: `https://thimodehaan.com:8080/cars/detail/`,
+      },
+    },
+  };
+}
 
-export {addCar, getAllCars, getAllBrands, getOneCar}
+export { addCar, getAllCars, getAllBrands, getOneCar };
