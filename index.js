ae0fc60d63f51e45d3504428ae62a4e621dd8b03
Thimo <thimodehaan@gmail.com>
2023-12-14 22:18:46 +0100

    changed detail headers

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Conten-Type, Accept');
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  // res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   if( req.headers?.accept === "application/json") {
     next()
   } else {
